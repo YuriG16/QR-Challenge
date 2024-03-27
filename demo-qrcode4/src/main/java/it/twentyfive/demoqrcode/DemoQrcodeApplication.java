cf07fef934958f4efa31540d45051ac04f682f4c
[200~YuriG16~ <grecoyuri@gmail.com>
2024-03-27 11:24:05 +0100

    feature/first-try-subclasses

---
+++ demo-qrcode4/src/main/java/it/twentyfive/demoqrcode/DemoQrcodeApplication.java
@@ -0,0 +1,13 @@
+package it.twentyfive.demoqrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQrcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoQrcodeApplication.class, args);
+    }
+
+}
