5475b072a340609ada3719d6a385abbde258529d
Dazi816 <davide.folino816@gmail.com>
2024-04-10 23:44:00 +0200

    modifica padding

--- QR-Customizable/demo-qrcode4/src/main/java/it/twentyfive/demoqrcode/model/CustomQrRequest.java
+++ QR-Customizable/demo-qrcode4/src/main/java/it/twentyfive/demoqrcode/model/CustomQrRequest.java
@@ -13,11 +13,15 @@ import lombok.NoArgsConstructor;
 public class CustomQrRequest {
     private int width;
     private int height;
+    private String padding;
     private String requestUrl;
     private CustomBord customBord;
     private CustomText customText;
     private CustomColor customColor;
     private LogoUrl logoUrl;
 
+    public int getPaddingInt() {
+        return Integer.parseInt(padding);
+    }
 }
 
