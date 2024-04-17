37c94c0a315fcd7986246f8a16ac315276cca610
Dazi816 <davide.folino816@gmail.com>
2024-04-17 23:33:53 +0200

    temp commit

--- QR-Customizable/demo-qrcode4/src/main/java/it/twentyfive/demoqrcode/utils/Exceptions/InvalidNumberException.java
+++
@@ -1,9 +0,0 @@
-package it.twentyfive.demoqrcode.utils.Exceptions;
-
-public class InvalidNumberException extends RuntimeException {
-    public InvalidNumberException() {
-    }
-    public InvalidNumberException(String message) {
-        super(message);
-    }
-}
--- QR-Customizable/demo-qrcode4/src/main/java/it/twentyfive/demoqrcode/utils/exceptions/InvalidNumberException.java
+++
@@ -1,9 +0,0 @@
-package it.twentyfive.demoqrcode.utils.exceptions;
-
-public class InvalidNumberException extends RuntimeException {
-    public InvalidNumberException() {
-    }
-    public InvalidNumberException(String message) {
-        super(message);
-    }
-}
